4f9d65b2802a8aa3cc4d6f49e39181abf2d14d01
SRIKANTH REDDY N <93464981+srikifreaky@users.noreply.github.com>
2022-12-06 11:14:05 +0530

    Add files via upload

---
+++ CalculatorOne/CalculatorOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using static System.Console;
+namespace CalculatorOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("---------- CALCULATOR ----------");
+            WriteLine("ENTER THE 1st NUMBER: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            WriteLine("ENTER THE 2ND NUMBER: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            WriteLine("SUM: {0}", add(a, b));
+        }
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
