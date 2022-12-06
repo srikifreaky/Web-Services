7e17ad1b477ff50d76076c5b0a0eeea8b205666b
SRIKANTH REDDY N <93464981+srikifreaky@users.noreply.github.com>
2022-12-06 11:20:12 +0530

    Add files via upload

---
+++ TemperatureConvertor/TemperatureConvertor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TemperatureConvertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("--------- TEMPERATURE CONVERTOR ---------\n");
+            Console.Write("CURRENT CELSIUS: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("KELVIN = {0}", celsius + 273);
+            Console.WriteLine("FAHRENHEIT = {0}", celsius * 18 / 10 + 32);
+        }
+    }
+}
